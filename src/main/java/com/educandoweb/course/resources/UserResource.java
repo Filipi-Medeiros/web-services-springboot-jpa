89def0a120ac3c8d39fef092dc8531f3309ffc3b
Filipi Medeiros <filipimedeiros112@gmail.com>
2024-11-12 21:33:27 -0300

    H2 database, test profile, JPA

--- src/main/java/com/educandoweb/course/resources/UserResource.java
+++ src/main/java/com/educandoweb/course/resources/UserResource.java
@@ -13,7 +13,7 @@ public class UserResource {
 	
 	@GetMapping
 	public ResponseEntity<User> findAll() {
-		User u = new User(1L, "Bob", "bob@gmail.com", "8888888", "12345");
+		User u = new User(1L, "Bob", "bob@gmail.com", "9999999", "12345");
 		return ResponseEntity.ok().body(u);
 	}
 }
